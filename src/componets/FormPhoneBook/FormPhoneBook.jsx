ad51dd0a7b158273e55dad2439ba3f9c7bdbfb0c
Iliya Lunev <lunevig90@gmail.com>
2021-10-18 14:46:34 +0300

    выполнен рефакторинг формы

--- src/componets/FormPhoneBook/FormPhoneBook.jsx
+++ src/componets/FormPhoneBook/FormPhoneBook.jsx
@@ -1,70 +1,71 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import s from './formPhone.module.css'
+import s from './formPhone.module.css';
 
-export default class FormPhoneBook extends Component {
-  state = {
-    name: '',
-    number: '',
-  };
+export default function FormPhoneBook({ onSubmit }) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  nameId = uuidv4();
-  numberId = uuidv4();
+  const nameId = uuidv4();
+  const numberId = uuidv4();
 
-  handleChange = e => {
+  const handleChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-    
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
   };
 
-  handleSubmit = e => {
-   e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
-    
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    onSubmit(name, number);
+    setName('');
+    setNumber('');
   };
 
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
-  render() {
-    const { name, number } = this.state;
-    
-
-    return (
-      <form className={s.form} onSubmit={this.handleSubmit}>
-        <label className={s.label} htmlFor={this.nameId}>
-          Name
-          <input
-            className={s.input}
-            type="text"
-            name="name"
-            id={this.nameId}
-            value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и
-            пробелов. Например Adrian, Jacob Mercer, Charles de Batz de
-            Castelmore d'Artagnan и т. п."
-            required
-            onChange={this.handleChange}
-          />
-        </label>
-        <label className={s.label} htmlFor={this.numberId}>
-          Number
-          <input
-            className={s.input}
-            type="tel"
-            name="number"
-            id={this.numberId}
-            value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
-            required
-            onChange={this.handleChange}
-          />
-        </label>
-        <button className={s.btn} type="submit">Add to contacts</button>
-      </form>
-    );
-  }
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.label} htmlFor={nameId}>
+        Name
+        <input
+          className={s.input}
+          type="text"
+          name="name"
+          id={nameId}
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и
+                пробелов. Например Adrian, Jacob Mercer, Charles de Batz de
+                Castelmore d'Artagnan и т. п."
+          required
+          onChange={handleChange}
+        />
+      </label>
+      <label className={s.label} htmlFor={numberId}>
+        Number
+        <input
+          className={s.input}
+          type="tel"
+          name="number"
+          id={numberId}
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          onChange={handleChange}
+        />
+      </label>
+      <button className={s.btn} type="submit">
+        Add to contacts
+      </button>
+    </form>
+  );
 }
